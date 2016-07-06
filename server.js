34fee0f49cabeb44cc427d38dce737d09cadacb7
MalvinMiao <lwmusc2012@gmail.com>
2016-07-06 15:21:33 -0700

    create individual jade files for about, blog, and others pages

--- server.js
+++ server.js
@@ -1,14 +1,44 @@
 var express = require('express')
   , logger = require('morgan')
   , app = express()
-  , template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
+  , homepage = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
+  , aboutpage = require('jade').compileFile(__dirname + '/source/templates/aboutpage.jade')
+  , blog = require('jade').compileFile(__dirname + '/source/templates/blog.jade')
+  , others = require('jade').compileFile(__dirname + '/source/templates/others.jade')
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
 
 app.get('/', function (req, res, next) {
   try {
-    var html = template({ title: 'Home' })
+    var html = homepage({ title: 'Home' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.get('/about', function(req, res, next) {
+  try {
+    var html = aboutpage({ title: 'About' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.get('/blog', function(req, res, next) {
+  try {
+    var html = blog({ title: 'Blog' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.get('/others', function(req, res, next) {
+  try {
+    var html = others({ title: 'Others' })
     res.send(html)
   } catch (e) {
     next(e)
