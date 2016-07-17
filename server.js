d2e057dcb52376a41bdf0c4166eb4cd04596e8f6
MalvinMiao <lwmusc2012@gmail.com>
2016-07-17 12:34:41 -0700

    add link to gfork-blog

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express')
   , logger = require('morgan')
   , app = express()
   , path = require('path')
-  , ghost = require('./ghost_app/ghost_in_the_middle')
   , homepage = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
   , aboutpage = require('jade').compileFile(__dirname + '/source/templates/aboutpage.jade')
   , others = require('jade').compileFile(__dirname + '/source/templates/others.jade')
@@ -30,10 +29,6 @@ app.get('/about', function(req, res, next) {
     next(e)
   }
 })
-
-app.use( '/blog', ghost({
-  config: path.join(__dirname, 'ghost_app/config.js')
-}) )
 
 app.get('/others', function(req, res, next) {
   try {
