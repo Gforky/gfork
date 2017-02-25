306afd663962a3796bb77907b79480fbcbab7e07
MarvinMiao <lwmusc2012@gmail.com>
2017-02-25 12:31:00 -0800

    add jquery

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var express = require('express')
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
+app.use(express.static(__dirname + '/node_modules'))
 
 
 app.get('/', function (req, res, next) {
